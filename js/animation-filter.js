321b9c0d28d32f6f7c0be1601ae3e324e85f147a
Dhruv-Nayar <nayardhruv0@gmail.com>
2016-04-21 00:52:31 -0400

    genre filters all added

--- js/animation-filter.js
+++ js/animation-filter.js
@@ -35,7 +35,21 @@ function moveDots(){
 		})
 		.attr("cy", function(d){
 			return yScale(d[yAxisUnit]) - 6;
-		});
+		})
+		.attr("opacity", function(d){
+			var contained = 0.1;
+			if(selectedGenres.length == 0)
+				return 1;
+			else{
+				d.genres.forEach(function(genre){
+					if(selectedGenres.indexOf(genre) != -1){
+						contained = 1
+						return false;
+					}
+				});
+				return contained;
+			}
+			});
 }
 
 //function that filters based on selected year, will be changed onces filterDot is implemented
@@ -58,11 +72,19 @@ function yearChange(year){
 				return "none";
 		})
 		.attr("opacity", function(d){
-			if (d.year == year)
+			var contained = 0.1;
+			if(selectedGenres.length == 0)
 				return 1;
-			else
-				return 0.3;
-		});
+			else{
+				d.genres.forEach(function(genre){
+					if(selectedGenres.indexOf(genre) != -1){
+						contained = 1
+						return false;
+					}
+				});
+				return contained;
+			}
+			});
 }
 
 //this function will be used in place of all of the filtering boolean expressions so we can check and combine multiple filters without having to write them into numerous places
@@ -76,16 +98,17 @@ function filterDots(){
 			return 0;
 		})
 		.attr("opacity", function(d){
+			var contained = 0.1;
 			if(selectedGenres.length == 0)
 				return 1;
 			else{
 				d.genres.forEach(function(genre){
 					if(selectedGenres.indexOf(genre) != -1){
-						console.log(genre);
-						return 1;
+						contained = 1
+						return false;
 					}
 				});
-				return 0;
+				return contained;
 			}
 			});
 				
@@ -99,5 +122,5 @@ function checkboxClicked(){
 			selectedGenres.push($(this).val());
 	});
 	console.log(selectedGenres);
-	//filterDots();
+	filterDots();
 }
